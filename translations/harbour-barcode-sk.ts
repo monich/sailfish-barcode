771045d4d01b9688aff5ea014b04324135fd6554
okruhliak <ladislav@hodas.sk>
2021-05-14 18:36:36 +0200

    Update harbour-barcode-sk.ts

---
+++ translations/harbour-barcode-sk.ts
@@ -0,0 +1,445 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name></name>
+    <message id="about-title">
+        <source>About CodeReader</source>
+        <extracomment>About page title, label and menu item</extracomment>
+        <translation>O aplikácii</translation>
+    </message>
+    <message id="about-description">
+        <source>This app demonstrates a bar code reader for Sailfish OS. We hope it is useful for other projects. CodeReader is open source and licensed under the MIT License.</source>
+        <extracomment>About page text</extracomment>
+        <translation>Táto aplikácia umožňuje čítať čiarové kódy v Sailfish OS. Snáď bude užitočná aj pre ďalšie projekty. CodeReader je softvér s otvoreným zdrojovým kódom a je poskytovaný pod licenciou MIT.</translation>
+    </message>
+    <message id="about-version-label">
+        <source>Version</source>
+        <extracomment>About page label</extracomment>
+        <translation>Veria</translation>
+    </message>
+    <message id="about-author-labels">
+        <source>Authors</source>
+        <extracomment>About page label</extracomment>
+        <translation>Autori</translation>
+    </message>
+    <message id="about-contributors-label">
+        <source>Contributors</source>
+        <extracomment>About page label</extracomment>
+        <translation>Prispievatelia</translation>
+    </message>
+    <message id="about-contributors-text">
+        <source>%1 and others.</source>
+        <extracomment>About page text</extracomment>
+        <translation>%1 a ďalší.</translation>
+    </message>
+    <message id="about-source_code-label">
+        <source>Source code</source>
+        <extracomment>About page label</extracomment>
+        <translation>Zdrojový kód</translation>
+    </message>
+    <message id="about-source_code-description">
+        <source>You can also file issues and bug reports there.</source>
+        <extracomment>About page description for source code URL</extracomment>
+        <translation>Môžete tiež nahlásiť problémy a chyby alebo navrhnúť zmeny.</translation>
+    </message>
+    <message id="about-references-label">
+        <source>References</source>
+        <extracomment>About page label</extracomment>
+        <translation>Odporúčania</translation>
+    </message>
+    <message id="about-references-text">
+        <source>This project uses code and ideas of other projects, see README.md on Github.</source>
+        <extracomment>About page text</extracomment>
+        <translation>Tento projekt využívá kód a myšlienky iných projektov, pozri README.md na Github.</translation>
+    </message>
+    <message id="about-supported_codes-label">
+        <source>Supported 1D/2D bar codes</source>
+        <extracomment>About page label</extracomment>
+        <translation>Podporované sú 1D/2D čiarové kódy</translation>
+    </message>
+    <message id="about-supported_codes-text">
+        <source>Image source: %1</source>
+        <extracomment>About page text</extracomment>
+        <translation>Zdroj obrázkov: %1</translation>
+    </message>
+    <message id="scan-slider-zoom">
+        <source>Zoom</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Piiblíženie</translation>
+    </message>
+    <message id="scan-action-scan">
+        <source>Scan</source>
+        <extracomment>Scan button label</extracomment>
+        <translation>Snímať</translation>
+    </message>
+    <message id="scan-action-cancel">
+        <source>Cancel</source>
+        <extracomment>Scan button label</extracomment>
+        <translation>Zrušit</translation>
+    </message>
+    <message id="scan-status-busy">
+        <source>Scan in progress ...</source>
+        <extracomment>Scan status label</extracomment>
+        <translation>Prebieha snímanie…</translation>
+    </message>
+    <message id="scan-status-nothing_found">
+        <source>No code detected! Try again.</source>
+        <extracomment>Scan status label</extracomment>
+        <translation>Nebol nájdený žiaden čiarový kód! Skúste to znova.</translation>
+    </message>
+    <message id="settings-title">
+        <source>Settings</source>
+        <extracomment>Setting page title and menu item</extracomment>
+        <translation>Nastavenia</translation>
+    </message>
+    <message id="settings-scan-section">
+        <source>Scan</source>
+        <extracomment>Section header</extracomment>
+        <translation>Snímanie</translation>
+    </message>
+    <message id="settings-sound-label">
+        <source>Detection sound</source>
+        <extracomment>Switch button text</extracomment>
+        <translation>Zvuk objavenia</translation>
+    </message>
+    <message id="settings-vibration-label">
+        <source>Vibrate on detection</source>
+        <extracomment>Switch button text</extracomment>
+        <translation>Vibrácie objavenia</translation>
+    </message>
+    <message id="settings-autoscan-label">
+        <source>Scan on start</source>
+        <extracomment>Switch button text</extracomment>
+        <translation>Snímať pri zapnutí</translation>
+    </message>
+    <message id="settings-volume_zoom-label">
+        <source>Zoom with volume keys</source>
+        <extracomment>Switch button text</extracomment>
+        <translation>Priblíženie pomocou tlačidiel hlasitosti</translation>
+    </message>
+    <message id="settings-save_images-label">
+        <source>Save barcode images</source>
+        <extracomment>Switch button text</extracomment>
+        <translation>Uložiť obrázok čiarového kódu</translation>
+    </message>
+    <message id="settings-save_images-description">
+        <source>This will delete all previously saved barcode images.</source>
+        <extracomment>Switch button description (explanation)</extracomment>
+        <translation>Toto vymaže všetky skôr uložené obrázky čiarových kódov.</translation>
+    </message>
+    <message id="settings-save_images-confirm_delete">
+        <source>Confirm delete</source>
+        <extracomment>Button label (confirm deletion of image files)</extracomment>
+        <translation>Potvrdiť vymazanie</translation>
+    </message>
+    <message id="settings-history-section">
+        <source>History</source>
+        <extracomment>Section header</extracomment>
+        <translation>História</translation>
+    </message>
+    <message id="settings-history-slider_label">
+        <source>Max history size (saved values: %1)</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Maximálna veľkosť histórie (uložených %1)</translation>
+    </message>
+    <message id="settings-value-deactivated">
+        <source>deactivated</source>
+        <extracomment>Generic slider value</extracomment>
+        <translation>vypnuté</translation>
+    </message>
+    <message id="settings-history-slider_value" numerus="yes">
+        <source>%1 item(s)</source>
+        <extracomment>History slider value</extracomment>
+        <translation>
+            <numerusform>%1 položka</numerusform>
+            <numerusform>%1 položky</numerusform>
+            <numerusform>%1 položiek</numerusform>
+        </translation>
+    </message>
+    <message id="settings-history-confirm_resize">
+        <source>Confirm resize</source>
+        <extracomment>Button label</extracomment>
+        <translation>Potvrdiť</translation>
+    </message>
+    <message id="settings-history-cancel">
+        <source>Cancel</source>
+        <extracomment>Button label</extracomment>
+        <translation>Zrušiť</translation>
+    </message>
+    <message id="settings-marker-section">
+        <source>Marker</source>
+        <extracomment>Section header</extracomment>
+        <translation>Označenie</translation>
+    </message>
+    <message id="settings-marker-slider_label">
+        <source>Mark detected code</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Označiť nájdený kód</translation>
+    </message>
+    <message id="settings-marker-slider_value" numerus="yes">
+        <source>%1 second(s)</source>
+        <extracomment>Marker slider value</extracomment>
+        <translation>
+            <numerusform>%1 sekunda</numerusform>
+            <numerusform>%1 sekundy</numerusform>
+            <numerusform>%1 sekúnd</numerusform>
+        </translation>
+    </message>
+    <message id="history-title">
+        <source>History</source>
+        <extracomment>History page title</extracomment>
+        <translation>História</translation>
+    </message>
+    <message id="history-menu-clear">
+        <source>Clear</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Vyčistiť</translation>
+    </message>
+    <message id="history-menu-select">
+        <source>Select</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Vybrať</translation>
+    </message>
+    <message id="history-menu-delete">
+        <source>Delete</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Zmazať</translation>
+    </message>
+    <message id="history-menu-copy">
+        <source>Copy to clipboard</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Kopírovať do schránky</translation>
+    </message>
+    <message id="history-menu-delete_remorse">
+        <source>Deleting</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>Mazanie</translation>
+    </message>
+    <message id="history-remorse-deleting_all">
+        <source>Deleting all codes</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>Mazanie všetkých kódov</translation>
+    </message>
+    <message id="history-remorse-deleting_selected_code">
+        <source>Deleting selected code</source>
+        <extracomment>Remorse popup text (single code selected)</extracomment>
+        <translation>Mazanie vybraného kódu</translation>
+    </message>
+    <message id="history-remorse-deleting_selected_codes">
+        <source>Deleting selected codes</source>
+        <extracomment>Remorse popup text (multiple codes selected)</extracomment>
+        <translation>Mazanie vybraných kódov</translation>
+    </message>
+    <message id="history-code_copied-notification">
+        <source>Selected code copied to clipboard</source>
+        <extracomment>Notification text (single code selected)</extracomment>
+        <translation>Vybraný kód bol skopírovaný do schránky</translation>
+    </message>
+    <message id="history-codes_copied-notification">
+        <source>Selected codes copied to clipboard</source>
+        <extracomment>Notification text (multiple codes selected)</extracomment>
+        <translation>Vybrané kódy boli skopírované do schránky</translation>
+    </message>
+    <message id="history-empty">
+        <source>History is empty</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>História je prázdná</translation>
+    </message>
+    <message id="text-open_link">
+        <source>Open link</source>
+        <extracomment>Button text</extracomment>
+        <translation>Otvoriť odkaz</translation>
+    </message>
+    <message id="text-open_url">
+        <source>Open URL</source>
+        <extracomment>Button text</extracomment>
+        <translation>Otvoriť URL</translation>
+    </message>
+    <message id="text-contact_card">
+        <source>Contact card</source>
+        <extracomment>Button text</extracomment>
+        <translation>Karta kontaktu</translation>
+    </message>
+    <message id="text-add_to_calendar">
+        <source>Add to calendar</source>
+        <extracomment>Button text</extracomment>
+        <translation>Pridať do kalendára</translation>
+    </message>
+    <message id="text-fetch_receipt">
+        <source>Fetch receipt</source>
+        <extracomment>Button text</extracomment>
+        <translation>Načítať bloček</translation>
+    </message>
+    <message id="text-fetching_receipt">
+        <source>Fetching...</source>
+        <extracomment>Button text</extracomment>
+        <translation>Načítanie…</translation>
+    </message>
+    <message id="text-cancel_fetching">
+        <source>Cancel</source>
+        <extracomment>Button label (cancel network operation)</extracomment>
+        <translation>Zrušiť</translation>
+    </message>
+    <message id="text-fetch_contacting">
+        <source>Contacting %1...</source>
+        <extracomment>Progress label</extracomment>
+        <translation>Pripájanie k %1…</translation>
+    </message>
+    <message id="text-receipt_not_found">
+        <source>Receipt not found</source>
+        <extracomment>Status label</extracomment>
+        <translation>Bloček nenájdený</translation>
+    </message>
+    <message id="text-network_error">
+        <source>Network error</source>
+        <extracomment>Status label</extracomment>
+        <translation>Chyba siete</translation>
+    </message>
+    <message id="contact-menu-save">
+        <source>Save</source>
+        <extracomment>Pulley menu item (saves contact)</extracomment>
+        <translation>Uložiť</translation>
+    </message>
+    <message id="contact-notification-saved">
+        <source>Saved contact</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Kontakt bol uložený</translation>
+    </message>
+    <message id="notification-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Skopírované do schránky</translation>
+    </message>
+    <message id="hint-toggle-flash">
+        <source>Toggle flashlight</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Prepnúť baterku</translation>
+    </message>
+    <message id="hint-aspect-ratio">
+        <source>Switch the aspect ratio between 9:16 and 3:4</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Prepínač pomeru strán medzi 9:16 a 3:4</translation>
+    </message>
+    <message id="hint-aspect-ratio_landscape">
+        <source>Switch the aspect ratio between 16:9 and 4:3</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Prepínač pomeru strán medzi 9:16 a 3:4</translation>
+    </message>
+    <message id="hint-copy-clipboard">
+        <source>Copy to clipboard</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Kopírovať do schránky</translation>
+    </message>
+    <message id="hint-open_contact_card">
+        <source>Open contact card</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Otvoriť kartu kontaktu</translation>
+    </message>
+    <message id="hint-open_link">
+        <source>Open link in browser</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Otvoriť odkaz v prehliadači</translation>
+    </message>
+    <message id="hint-open_url">
+        <source>Open URL in default application</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Otvoriť URL v implicitnej aplikácii</translation>
+    </message>
+    <message id="settings-display-section">
+        <source>Display</source>
+        <extracomment>Section header</extracomment>
+        <translation>Displej</translation>
+    </message>
+    <message id="settings-display-orientation-label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Orientácia</translation>
+    </message>
+    <message id="settings-display-orientation-primary">
+        <source>Primary</source>
+        <extracomment>Combo box value for primary orientation</extracomment>
+        <translation>Hlavný</translation>
+    </message>
+    <message id="settings-display-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Na výšku</translation>
+    </message>
+    <message id="settings-display-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Na šírku</translation>
+    </message>
+    <message id="settings-display-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dynamicky</translation>
+    </message>
+    <message id="settings-display-orientation-allow_inverted">
+        <source>Allow inverted orientation</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Povolit obrátenú orientáciu</translation>
+    </message>
+    <message id="settings-display-orientation-allow_inverted-description">
+        <source>If enabled, allows both primary and inverted landscape or portrait orientation.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Ak je povolené, umožňuje tak hlavnú, ako aj invertovanú orientáciu na šířku alebo na výšku.</translation>
+    </message>
+    <message id="select-title">
+        <source>Select codes</source>
+        <extracomment>Selection page title</extracomment>
+        <translation>Vybrať kódy</translation>
+    </message>
+    <message id="select-menu-all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Vybrať všetky</translation>
+    </message>
+    <message id="select-menu-none">
+        <source>Select none</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Zrušiť výber</translation>
+    </message>
+    <message id="hint-delete_selected_codes">
+        <source>Delete selected codes</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Odstrániť vybrané kódy</translation>
+    </message>
+    <message id="hint-copy_selected_codes">
+        <source>Copy selected codes to clipboard</source>
+        <extracomment>Hint label</extracomment>
+        <translation>Kopírovať vybrané kódy do schránky</translation>
+    </message>
+    <message id="hint-history_swipe">
+        <source>Swipe to see other history entries</source>
+        <extracomment>Hint text for a swipe (either left or right)</extracomment>
+        <translation>Potiahnutím zobraziť ďalšie záznamy</translation>
+    </message>
+    <message id="scan-gallery-menu">
+        <source>Scan from Gallery</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Snímať z galérie</translation>
+    </message>
+    <message id="gallery-title">
+        <source>Select image</source>
+        <extracomment>Page header</extracomment>
+        <translation>Vybrať obrázok</translation>
+    </message>
+    <message id="gallery-empty">
+        <source>No images found in the gallery</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation type="unfinished">V galérii nie je žiaden obrázok</translation>
+    </message>
+    <message id="gallery-thumbnail-error">
+        <source>Error</source>
+        <extracomment>Thumbnail image loading failed</extracomment>
+        <translation>Chyba</translation>
+    </message>
+    <message id="gallery-rotate">
+        <source>Rotate</source>
+        <extracomment>Rotation widget</extracomment>
+        <translation>Otočiť</translation>
+    </message>
+</context>
+</TS>
